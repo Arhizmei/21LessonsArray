42eddf95b9c13a85fb0bd2c3938c9eab2f92e7ef
Arhizmei <arhizmei@gmail.com>
2023-03-23 10:11:11 +0200

    1.2

--- app/src/main/java/com/zmei/a21lessonsarray/MainActivity.kt
+++ app/src/main/java/com/zmei/a21lessonsarray/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var bind : ActivityMainBinding
     var invest = arrayOf(1233, 4444, 2222, 5555, 90000, 550)
     var earn = arrayOf(1200, 33, 234,44,22222,4444)
+    var resultList = arrayListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         bind = ActivityMainBinding.inflate(layoutInflater)
@@ -16,5 +17,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(bind.root)
 
         val names = resources.getStringArray(R.array.names)
+
+        for ((index, name) in names.withIndex()){
+
+            resultList.add("Name: $name - earn = ${earn[index]-invest[index]}")
+
+
+        }
+        bind.textView.text = resultList[0]
+        bind.textView2.text = resultList[1]
+        bind.textView3.text = resultList[2]
+        bind.textView4.text = resultList[3]
+        bind.textView5.text = resultList[4]
+        bind.textView6.text = resultList[5]
     }
 }
